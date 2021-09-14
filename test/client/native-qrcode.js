b341d703dbe39c51a4227aeb72b3a9a465e546bb
fedperez <fedperez@paypal.com>
2021-09-14 18:33:46 -0300

    escape path test

--- test/client/native-qrcode.js
+++ test/client/native-qrcode.js
@@ -626,5 +626,126 @@ describe('native qrcode cases', () => {
                 gqlMock.done();
             });
         });
+
+        it('should render a button with createOrder, click the button, and render checkout via qrcode that contains escape path with empty ineligibilityReason and escapepath', async () => {
+            return await wrapPromise(async ({ expect, avoid }) => {
+                window.xprops.platform = PLATFORM.DESKTOP;
+                delete window.xprops.onClick;
+    
+                const sessionToken = uniqueID();
+                const payerID = 'XXYYZZ654321';
+    
+                const gqlMock = getGraphQLApiMock({
+                    extraHandler: expect('firebaseGQLCall', ({ data }) => {
+                        if (data.query.includes('query GetNativeEligibility')) {
+                            return {
+                                data: {
+                                    mobileSDKEligibility: {
+                                        paypal: {
+                                            eligibility: true
+                                        },
+                                        venmo: {
+                                            eligibility:         false,
+                                            ineligibilityReason: ''
+                                        }
+                                    }
+                                }
+                            };
+                        }
+    
+                        if (!data.query.includes('query GetFireBaseSessionToken')) {
+                            return;
+                        }
+    
+                        if (!data.variables.sessionUID) {
+                            throw new Error(`Expected sessionUID to be passed`);
+                        }
+    
+                        return {
+                            data: {
+                                firebase: {
+                                    auth: {
+                                        sessionUID: data.variables.sessionUID,
+                                        sessionToken
+                                    }
+                                }
+                            }
+                        };
+                    })
+                }).expectCalls();
+    
+                let mockWebSocketServer;
+    
+                mockFunction(window.paypal, 'QRCode', expect('QRCode', ({ original, args: [ props ] }) => {
+                    const query = parseQuery(props.qrPath.split('?')[1]);
+    
+                    const { expect: expectSocket, onInit, onApprove } = getNativeFirebaseMock({
+                        sessionUID:   query.sessionUID
+                    });
+    
+                    mockWebSocketServer = expectSocket();
+                    
+                    ZalgoPromise.try(() => {
+                        return onInit();
+                    }).then(() => {
+                        return onApprove({ payerID });
+                    });
+    
+                    return original(props);
+                }));
+    
+                const orderID = generateOrderID();
+    
+                window.xprops.createOrder = mockAsyncProp(expect('createOrder', async () => {
+                    return ZalgoPromise.try(() => {
+                        return orderID;
+                    });
+                }), 50);
+    
+                window.xprops.onCancel = avoid('onCancel');
+    
+                window.xprops.onApprove = mockAsyncProp(expect('onApprove', (data) => {
+                    if (data.orderID !== orderID) {
+                        throw new Error(`Expected orderID to be ${ orderID }, got ${ data.orderID }`);
+                    }
+    
+                    if (data.payerID !== payerID) {
+                        throw new Error(`Expected payerID to be ${ payerID }, got ${ data.payerID }`);
+                    }
+                }));
+
+                window.xprops.onEscapePath = mockAsyncProp(expect('onEscapePath', (selectedFundingSource) => {
+                    if (selectedFundingSource !== FUNDING.CARD) {
+                        throw new Error(`Expected selectedFundingSource  to be ${ FUNDING.CARD }, got ${ selectedFundingSource }`);
+                    }
+                }));
+    
+                const fundingEligibility = {
+                    venmo: {
+                        eligible: true
+                    }
+                };
+    
+                createButtonHTML({ fundingEligibility });
+    
+                await mockSetupButton({
+                    eligibility: {
+                        cardFields: false,
+                        native:     true
+                    }
+                });
+    
+                await clickButton(FUNDING.VENMO);
+
+                await window.xprops.onEscapePath(FUNDING.CARD);
+                await window.xprops.onApprove.await();
+    
+                if (mockWebSocketServer) {
+                    mockWebSocketServer.done();
+                }
+                
+                gqlMock.done();
+            });
+        });
     });
 });
