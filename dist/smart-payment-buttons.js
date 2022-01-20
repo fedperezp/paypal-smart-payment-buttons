f6c2b3e657064218e1a8aeed63b34e0d59deb67f
fedperez <fedperez@paypal.com>
2022-01-20 11:29:20 -0300

    revert alpha testing -> dists

--- dist/smart-payment-buttons.js
+++ dist/smart-payment-buttons.js
@@ -7084,43 +7084,6 @@ window.spb = function(modules) {
                 throw err;
             }));
         }
-        var _FRAUDNET_URL;
-        var FRAUDNET_URL = ((_FRAUDNET_URL = {}).local = "https://www.stage2d0107.stage.paypal.com/FDRegression/fb.js", 
-        _FRAUDNET_URL.stage = "https://www.stage2d0107.stage.paypal.com/FDRegression/fb.js", 
-        _FRAUDNET_URL.sandbox = "https://c.paypal.com/da/r/fb.js", _FRAUDNET_URL.production = "https://c.paypal.com/da/r/fb.js", 
-        _FRAUDNET_URL.test = "https://c.paypal.com/da/r/fb.js", _FRAUDNET_URL);
-        var loadFraudnet = memoize((function(_ref) {
-            var env = _ref.env, clientMetadataID = _ref.clientMetadataID, cspNonce = _ref.cspNonce, _ref$timeout = _ref.timeout, timeout = void 0 === _ref$timeout ? 1e3 : _ref$timeout;
-            return new promise_ZalgoPromise((function(resolve) {
-                var config = {
-                    f: clientMetadataID,
-                    s: "SMART_PAYMENT_BUTTONS",
-                    cb1: "fnCallback"
-                };
-                "sandbox" === env && (config.sandbox = !0);
-                var configScript = document.createElement("script");
-                configScript.setAttribute("nonce", cspNonce || "");
-                configScript.setAttribute("type", "application/json");
-                configScript.setAttribute("id", "fconfig");
-                configScript.setAttribute("fncls", "fnparams-dede7cc5-15fd-4c75-a9f4-36c430ee3a99");
-                configScript.textContent = JSON.stringify(config);
-                var fraudnetScript = document.createElement("script");
-                fraudnetScript.setAttribute("nonce", cspNonce || "");
-                fraudnetScript.setAttribute("src", FRAUDNET_URL[env]);
-                fraudnetScript.addEventListener("error", (function() {
-                    return resolve();
-                }));
-                window.fnCallback = resolve;
-                setTimeout(resolve, timeout);
-                var body = function() {
-                    var body = document.body;
-                    if (!body) throw new Error("Document body not found");
-                    return body;
-                }();
-                body.appendChild(configScript);
-                body.appendChild(fraudnetScript);
-            }));
-        }));
         function isProcessorDeclineError(err) {
             var _err$response, _err$response$body, _err$response$body$da, _err$response$body$da2;
             return Boolean(null == err || null == (_err$response = err.response) || null == (_err$response$body = _err$response.body) || null == (_err$response$body$da = _err$response$body.data) || null == (_err$response$body$da2 = _err$response$body$da.details) ? void 0 : _err$response$body$da2.some((function(detail) {
@@ -7205,7 +7168,7 @@ window.spb = function(modules) {
             logger_getLogger().info("rest_api_create_order_token");
             var headers = ((_headers15 = {}).authorization = "Bearer " + accessToken, _headers15["paypal-partner-attribution-id"] = partnerAttributionID, 
             _headers15["paypal-client-metadata-id"] = clientMetadataID, _headers15["x-app-name"] = "smart-payment-buttons", 
-            _headers15["x-app-version"] = "5.0.79", _headers15);
+            _headers15["x-app-version"] = "5.0.78", _headers15);
             var paymentSource = {
                 token: {
                     id: paymentMethodID,
@@ -7250,17 +7213,16 @@ window.spb = function(modules) {
         }
         function oneClickApproveOrder(_ref20) {
             var _headers20;
-            var orderID = _ref20.orderID, instrumentType = _ref20.instrumentType, instrumentID = _ref20.instrumentID, buyerAccessToken = _ref20.buyerAccessToken, clientMetadataID = _ref20.clientMetadataID;
-            window.PAYPAL && window.PAYPAL.asyncData && "function" == typeof window.PAYPAL.asyncData.initAndCollect && window.PAYPAL.asyncData.initAndCollect();
+            var orderID = _ref20.orderID, clientMetadataID = _ref20.clientMetadataID;
             return callGraphQL({
                 name: "OneClickApproveOrder",
                 query: "\n            mutation OneClickApproveOrder(\n                $orderID : String!\n                $instrumentType : String!\n                $instrumentID : String!\n            ) {\n                oneClickPayment(\n                    token: $orderID\n                    selectedInstrumentType : $instrumentType\n                    selectedInstrumentId : $instrumentID\n                ) {\n                    userId\n                }\n            }\n        ",
                 variables: {
                     orderID: orderID,
-                    instrumentType: instrumentType,
-                    instrumentID: instrumentID
+                    instrumentType: _ref20.instrumentType,
+                    instrumentID: _ref20.instrumentID
                 },
-                headers: (_headers20 = {}, _headers20["x-paypal-internal-euat"] = buyerAccessToken, 
+                headers: (_headers20 = {}, _headers20["x-paypal-internal-euat"] = _ref20.buyerAccessToken, 
                 _headers20["paypal-client-context"] = orderID, _headers20["paypal-client-metadata-id"] = clientMetadataID || orderID, 
                 _headers20)
             }).then((function(_ref21) {
@@ -7362,6 +7324,43 @@ window.spb = function(modules) {
                 if (!firebase) throw new Error("Firebase failed to load");
                 firebase.initializeApp(config);
                 return firebase;
+            }));
+        }));
+        var _FRAUDNET_URL;
+        var FRAUDNET_URL = ((_FRAUDNET_URL = {}).local = "https://www.stage2d0107.stage.paypal.com/FDRegression/fb.js", 
+        _FRAUDNET_URL.stage = "https://www.stage2d0107.stage.paypal.com/FDRegression/fb.js", 
+        _FRAUDNET_URL.sandbox = "https://c.paypal.com/da/r/fb.js", _FRAUDNET_URL.production = "https://c.paypal.com/da/r/fb.js", 
+        _FRAUDNET_URL.test = "https://c.paypal.com/da/r/fb.js", _FRAUDNET_URL);
+        var loadFraudnet = memoize((function(_ref) {
+            var env = _ref.env, clientMetadataID = _ref.clientMetadataID, cspNonce = _ref.cspNonce, _ref$timeout = _ref.timeout, timeout = void 0 === _ref$timeout ? 1e3 : _ref$timeout;
+            return new promise_ZalgoPromise((function(resolve) {
+                var config = {
+                    f: clientMetadataID,
+                    s: "SMART_PAYMENT_BUTTONS",
+                    cb1: "fnCallback"
+                };
+                "sandbox" === env && (config.sandbox = !0);
+                var configScript = document.createElement("script");
+                configScript.setAttribute("nonce", cspNonce || "");
+                configScript.setAttribute("type", "application/json");
+                configScript.setAttribute("id", "fconfig");
+                configScript.setAttribute("fncls", "fnparams-dede7cc5-15fd-4c75-a9f4-36c430ee3a99");
+                configScript.textContent = JSON.stringify(config);
+                var fraudnetScript = document.createElement("script");
+                fraudnetScript.setAttribute("nonce", cspNonce || "");
+                fraudnetScript.setAttribute("src", FRAUDNET_URL[env]);
+                fraudnetScript.addEventListener("error", (function() {
+                    return resolve();
+                }));
+                window.fnCallback = resolve;
+                setTimeout(resolve, timeout);
+                var body = function() {
+                    var body = document.body;
+                    if (!body) throw new Error("Document body not found");
+                    return body;
+                }();
+                body.appendChild(configScript);
+                body.appendChild(fraudnetScript);
             }));
         }));
         var getSmartWallet = memoize((function(_ref) {
@@ -8838,7 +8837,7 @@ window.spb = function(modules) {
                                                 label: "Shipping",
                                                 amount: shippingValue
                                             });
-                                            selectedShippingMethod && "PICKUP" === selectedShippingMethod.type && (result.requiredShippingContactFields = []);
+                                            (!selectedShippingMethod || selectedShippingMethod && "PICKUP" === selectedShippingMethod.type) && (result.requiredShippingContactFields = []);
                                             return result;
                                         }(country, order);
                                         var _order$checkoutSessio = order.checkoutSession.cart.amounts, shippingValue = _order$checkoutSessio.shippingAndHandling.currencyValue, taxValue = _order$checkoutSessio.tax.currencyValue, subtotalValue = _order$checkoutSessio.subtotal.currencyValue;
@@ -13417,7 +13416,7 @@ window.spb = function(modules) {
                 logger.addTrackingBuilder((function() {
                     var _ref3;
                     return (_ref3 = {}).state_name = "smart_button", _ref3.context_type = "button_session_id", 
-                    _ref3.context_id = buttonSessionID, _ref3.button_session_id = buttonSessionID, _ref3.button_version = "5.0.79", 
+                    _ref3.context_id = buttonSessionID, _ref3.button_session_id = buttonSessionID, _ref3.button_version = "5.0.78", 
                     _ref3.button_correlation_id = buttonCorrelationID, _ref3.stickiness_id = isAndroidChrome() ? stickinessID : null, 
                     _ref3.bn_code = partnerAttributionID, _ref3.user_action = commit ? "commit" : "continue", 
                     _ref3.seller_id = merchantID[0], _ref3.merchant_domain = merchantDomain, _ref3.t = Date.now().toString(), 
@@ -13524,7 +13523,7 @@ window.spb = function(modules) {
                 components: components
             });
             var setupExportsTask = function(_ref) {
-                var props = _ref.props, isEnabled = _ref.isEnabled, facilitatorAccessToken = _ref.facilitatorAccessToken;
+                var props = _ref.props, isEnabled = _ref.isEnabled, facilitatorAccessToken = _ref.facilitatorAccessToken, fundingEligibility = _ref.fundingEligibility, merchantID = _ref.merchantID;
                 var _createOrder = props.createOrder, _onApprove = props.onApprove, onError = props.onError, onCancel = props.onCancel, onClick = props.onClick, commit = props.commit, intent = props.intent, fundingSource = props.fundingSource, currency = props.currency;
                 var fundingSources = querySelectorAll("[data-funding-source]").map((function(el) {
                     return el.getAttribute("data-funding-source");
@@ -13535,6 +13534,23 @@ window.spb = function(modules) {
                         commit: commit,
                         currency: currency,
                         intent: intent
+                    },
+                    currency: currency,
+                    intent: intent,
+                    isGuestEnabled: function() {
+                        var _fundingEligibility$c;
+                        return null != fundingEligibility && null != (_fundingEligibility$c = fundingEligibility.card) && _fundingEligibility$c.hasOwnProperty("guestEnabled") ? fundingEligibility.card.guestEnabled : function(merchantID) {
+                            return callGraphQL({
+                                name: "GetFundingEligibility",
+                                query: "\n            query GetFundingEligibility(\n                $merchantID:[ String ]\n            ) {\n            fundingEligibility(\n                merchantId: $merchantID\n            ) {\n                card {\n                    guestEnabled\n                }\n            }\n          }\n        ",
+                                variables: {
+                                    merchantID: merchantID
+                                }
+                            }).then((function(gqlResult) {
+                                if (!gqlResult || !gqlResult.fundingEligibility) throw new Error("GraphQL fundingEligibility returned no fundingEligibility object");
+                                return gqlResult && gqlResult.fundingEligibility && gqlResult.fundingEligibility.card && gqlResult.fundingEligibility.card.guestEnabled;
+                            }));
+                        }(merchantID);
                     },
                     paymentSession: function() {
                         return {
@@ -13587,7 +13603,9 @@ window.spb = function(modules) {
             }({
                 props: props,
                 isEnabled: isEnabled,
-                facilitatorAccessToken: facilitatorAccessToken
+                facilitatorAccessToken: facilitatorAccessToken,
+                fundingEligibility: fundingEligibility,
+                merchantID: merchantID
             });
             var validatePropsTask = setupButtonLogsTask.then((function() {
                 return function(_ref2) {
